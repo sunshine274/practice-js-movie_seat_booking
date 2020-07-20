d840e1df46bf04ebd12a9ee80284146c5a976c96
sunshine274 <jingx274@gmail.com>
2020-07-20 11:26:24 +0000

    Added seat selected functions

---
+++ script.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+//Update total and count
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat) =>  [...seats].indexOf(seat)
+    );
+
+   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    // Copy selected seats into arr
+    // Map through array
+    // Return a new array indexes
+
+
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Movie select event
+movieSelect.addEventListener('change', e =>{
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+
+//Seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount()
+  }
+});
